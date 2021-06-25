5fac6d07f4de3f5de2a3d7a99c9fb4914a75ceb3
Piotr Kukla <piotr.kukla@picadilla.com>
2021-06-25 08:24:14 +0200

    unused code

--- src/main/scala/com/kuki/flightfinder/ListAirportsExample.scala
+++ src/main/scala/com/kuki/flightfinder/ListAirportsExample.scala
@@ -1,19 +1,10 @@
 package com.kuki.flightfinder
 
 import zio._
-import zio.blocking._
 import zio.console._
 
-import scala.io.{Codec, Source}
 
 object ListAirportsExample extends App {
-
-  def getResource(path: String): ZIO[Blocking, Throwable, Iterator[String]] = effectBlocking {
-    Source.fromResource(path)(Codec.UTF8).getLines
-  }
-
-
-
 
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] =
     (for {
